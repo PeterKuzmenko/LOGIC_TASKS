bec2e1f82b1fcdee1234ac7b09e8043ce79d84fb
PeterKuzmenko <petr.kuzmenko.04@gmail.com>
2021-07-27 12:27:27 +0300

    Finish fifth task

---
+++ task-5/script-2.js
@@ -0,0 +1,18 @@
+'use strict'
+
+function wait() {
+    return new Promise(function(resolve, reject) {
+      setTimeout(() => resolve(1), 1000);
+    })
+}
+
+async function f() {
+    try {
+        const data = await wait();
+        console.log(data);
+    } catch(e) {
+        console.log(e);
+    } finally {
+        console.log('Promise complete');
+    }
+}
